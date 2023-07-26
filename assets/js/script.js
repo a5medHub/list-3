e1188b1d9148a0825cc2317b90648e76c6e745c6
A.Yassine <ayassine.auce@gmail.com>
2023-07-26 20:37:34 +0300

    add else

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,9 @@ loginBtn.addEventListener("click", function(){
     for (let i =0; i<=accounts.length; i++){
         // console.log(accounts[i][0])
         if (accessUser ==  accounts[i][0] && accessPass == accounts[i][1]){
-            console.log(`welcome ${accessUser}`)
+            alert(`welcome ${accessUser}`)
+        }else{
+            alert(`incorrect username or password`)
         }
     }
     
