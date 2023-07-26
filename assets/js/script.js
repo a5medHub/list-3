7321341c3a125efaec386fbb57436d148256fde6
A.Yassine <ayassine.auce@gmail.com>
2023-07-26 19:56:49 +0300

    change let to const

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
-let loginBtn = document.getElementById("login")
-let usernameEl = document.getElementById("username")
-let passwordEl = document.getElementById("password")
+
+const loginBtn = document.getElementById("login")
+const usernameEl = document.getElementById("username")
+const passwordEl = document.getElementById("password")
 document.title = ("LogIn Page")
 
 loginBtn.addEventListener("click", function(){
     let accessUser = usernameEl.value
     let accessPass = passwordEl.value
-    console.log(accessUser)
+    // console.log(accessUser, accessPass)
+    // if (accessUser == "admin01" && accessPass =="123"){
+
+    // } else if (accessUser == "admin01" && accessPass =="123"){
+
+    // }
 })
