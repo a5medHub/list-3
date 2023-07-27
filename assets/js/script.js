01e2d1e671baf34a7810d7a1d7dc3425c36c8692
A.Yassine <ayassine.auce@gmail.com>
2023-07-27 20:10:13 +0300

    add counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,24 +3,28 @@ const loginBtn = document.getElementById("login")
 const usernameEl = document.getElementById("username")
 const passwordEl = document.getElementById("password")
 document.title = ("LogIn Page")
-
+let counter = 1
 loginBtn.addEventListener("click", function(){
-    let accessUser = usernameEl.value
-    let accessPass = passwordEl.value
-    let found = false
-    for (let i in accounts){
-        if (accessUser ==  accounts[i][0] && accessPass == accounts[i][1]){
-            found = true
-            document.write(`<h1 style="text-align:center; color:blue; margin-top:50px;"> welcome 
-                <span style="font-weight:bold; color:red;">
-                ${accessUser.toUpperCase()}</span> </h1>`)
-                setTimeout(secondPage,2000)
-                
-
+    if(counter < 4) {
+        let accessUser = usernameEl.value
+        let accessPass = passwordEl.value
+        let found = false
+        for (let i in accounts){
+            if (accessUser ==  accounts[i][0] && accessPass == accounts[i][1]){
+                found = true
+                counter = 1
+                document.write(`<h1 style="text-align:center; color:blue; margin-top:50px;"> welcome 
+                    <span style="font-weight:bold; color:red;">
+                    ${accessUser.toUpperCase()}</span> </h1>`)
+                    setTimeout(secondPage,2000)
+            }
         }
-    }
-    if (found == false) {
-        alert('Invalid Username or Password')
+        if (found == false) {
+            counter++
+            alert('Invalid Username or Password')
+        }
+    }else{
+        alert("Maximum Attempts Exceeded!")
     }
 })
 function secondPage(){
